0084f14cdd1134572096513a59ebf996be8a6361
Jacob Chwastek <jacobchwastek@gmail.com>
2020-05-20 21:15:28 +0200

    Font size adjustment

--- SystemUsageDisplay.py
+++ SystemUsageDisplay.py
@@ -2,17 +2,22 @@ from SystemVolume import show_mixer
 from SystemUsage import show_host_data
 
 from tkinter import Label, font, Frame
-
+import time
 
 
 def init_gui(frame):
+
+    # testowe wywołanie
+    # todo usunąć testowe wywołanie
     text = set_data_usage(show_host_data())
-    title = Label(frame, text="System usage", font=font.Font(size=30))
+        
+
+    title = Label(frame, text="System usage", font=font.Font(size=20))
     title.grid(column=0, row=0)
 
     usage_frame = Frame(frame)
     usage_frame.grid(column=0, row=1)
-    screen = Label(usage_frame, text=text, width=40, height=2, bg="#0b8615")
+    screen = Label(usage_frame, text=text ,font=10, width=40, height=2, bg="#0b8615")
     screen.grid(column=1, row=0)
     usage_frame.grid_columnconfigure(1, minsize=40)
 
@@ -22,7 +27,7 @@ def set_data_usage(dic):
     text ="Cpu: "
     text += str(dic['cpu']) + "%"
     text += " Temperature: "
-    text += str(dic['temperature']) + "C"
+    text += str(dic['temperature']) + '\N{DEGREE SIGN}C'
     text += " Memory "
     text += str(dic['memory']) + "%"
     return text
