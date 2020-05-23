e79688bf705690ea4b59ab7e2b2e518e0abd0886
Jacob Chwastek <jacobchwastek@gmail.com>
2020-05-23 21:32:28 +0200

    test

--- Buttons_Description.py
+++ Buttons_Description.py
@@ -5,7 +5,6 @@ import requests
 import time
 import RPi.GPIO as GPIO
 
-#display wired in 8 bit mode
 lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[40, 38, 36, 32, 33, 31, 29, 23])
 
 Pin1 = 3
